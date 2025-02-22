1fdaa79acc18624b1d8edb43bdf4dee904dc53d7
manishroy09 <manishroy2163@gmail.com>
2025-02-22 18:55:50 +0530

    22nd feb 2025

--- secondclass/src/App.jsx
+++ secondclass/src/App.jsx
@@ -3,8 +3,13 @@ function App() {
  
 
   return (
+    <>
    <h1>  Welcom to React js class</h1>
-     
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, vero?</p>
+   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt eius cumque eveniet ut aperiam magni deserunt, distinctio animi nulla blanditiis minus ipsum dolores quas neque eaque, enim, repellat quisquam numquam. Similique ea vitae sunt enim tempora. Quam ad sint nesciunt possimus labore consequatur velit asperiores nobis maiores aliquid. Non, nisi.</p>
+   <img src="nature.jpg" alt="" height="500px" />
+   </>
+    
   )
 }
 
