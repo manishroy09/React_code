304852d1b30fe3b3c1e5e9321897e092d72d4479
manishroy09 <manishroy2163@gmail.com>
2025-03-04 18:58:43 +0530

    4th march 2025

---
+++ states/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+
+
+
+function App (){
+  let [name, setName] = useState("Manish kumar")
+
+  function fun(){
+    setName("Yuvraj roy")
+  }
+  
+  let [age, setAge] = useState(21)
+
+  let [contact, setContact] =  useState(12334)
+  function fcontact(){
+    setContact(6206743012)
+  }
+  let [color,ChangeColor] = useState("blue")
+
+  return(
+    <>
+    <div style={{ height:"100vh",backgroundColor:color}}>
+    <h1>My name is {name} </h1>
+    <button onClick={fun}>Change name</button>
+
+    <h1>My age is {age}</h1>
+    <button onClick={()=>setAge(90)}>Change age</button>
+
+    <h1>My contact no is {contact}</h1>
+    <button onClick={fcontact}>Change contact</button>
+
+    <button onClick={()=>ChangeColor("red")}>Red</button>
+    <button onClick={()=>ChangeColor("yellow")}>Yellow</button>
+    <button onClick={()=>ChangeColor("pink")}>Pink</button>
+    <button onClick={()=>ChangeColor("lightblue")}>light Blue</button>
+    </div>
+    </>
+  )
+}
+
+export default App
+
