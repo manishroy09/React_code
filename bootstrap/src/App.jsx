ca07af532435de1bba4e223af042bd078bf757cc
manishroy09 <manishroy2163@gmail.com>
2025-03-10 18:59:53 +0530

    10th march 2025

---
+++ bootstrap/src/App.jsx
@@ -0,0 +1,22 @@
+import { Container,Col, Row } from 'react-bootstrap'
+
+
+
+function App (){
+  return(
+    <>
+    <Container>
+      <Row>
+      <Col>
+      <h1>First Column</h1>
+      </Col>
+      <Col>
+      <h1>Second Column</h1>
+      </Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
+
+export default App
