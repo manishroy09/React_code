c9beddf888c3a7e5c8deac4f5f9a25317efe5278
manishroy09 <manishroy2163@gmail.com>
2025-04-12 09:48:46 +0530

    12th april 2025

---
+++ map/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import myimg from './assets/react.svg'
+
+const App = () => {
+
+  let product = [
+    {
+      id:1, pname:"Mobile", pprice:12000, pimg:myimg
+    },
+    {
+      id:2, pname:"Laptop", pprice:90000, pimg:myimg
+    }
+  ]
+  return (
+    <>
+    <div style={{display:"flex", gap:"20px"}}>
+        {
+          product.map((e)=>{return <div style={{border:"10px soldi black",backgroundColor:"goldenrod", padding:"20px"}}>
+            <h1>Product Name : {e.pname} </h1>
+            <h1>Product Price : {e.pprice} </h1>
+            <img src={e.pimg} alt="" /><br />
+            <button>Add to cart</button>
+          </div>})
+        }
+    </div>
+    </>
+  )
+}
+
+export default App
