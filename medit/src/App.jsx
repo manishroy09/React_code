6448d6dc17059fdb0203bc3dacd3664c2d701be1
manishroy09 <manishroy2163@gmail.com>
2025-05-08 13:10:56 +0530

    8th may 2025

--- medit/src/App.jsx
+++ medit/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
+import { Navbar } from 'react-bootstrap'
 
 function App() {
   
 
   return (
     <>
-     
+     <Navbar/>
     </>
   )
 }
